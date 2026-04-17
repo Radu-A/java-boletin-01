baf11b7631b5cfaa7bbf8fcccfd55377ea498b61
Radu-A <elcastravete@gmail.com>
2026-04-17 11:07:56 +0200

    vector: finish

--- src/primerasclases/TestVector.java
+++ src/primerasclases/TestVector.java
@@ -5,6 +5,24 @@ public class TestVector {
 	public static void main(String[] args) {
 		Vector v1 = new Vector(5, 6);
 		v1.show();
+		
+		Vector v2 = new Vector(v1);
+		v2.show();
+		
+		v1.opera(v2, '+');
+		v1.opera(v2, '-');
+		v1.opera(v2, '*');
+		
+		System.out.println(v1.prod(2));
+		v1.opera(2);
+		
+		Vector v3 = new Vector(2, 3);
+		Vector v4 = new Vector(-3, 2);
+		System.out.println(v3.esPerpendicular(v4));
+		
+		Vector v5 = new Vector(0, 0);
+		Vector v6 = new Vector(3, 0);
+		System.out.println(v5.esParalelo(v6));
 	}
 
 }
--- src/primerasclases/Vector.java
+++ src/primerasclases/Vector.java
@@ -22,4 +22,70 @@ public class Vector {
 	public void show() {
 		System.out.printf("Vector de coordenadas (%d, %d)%n", this.x, this.y);
 	}
+	
+	public int getX() {
+		return this.x;
+	}
+	public int getY() {
+		return this.y;
+	}
+	
+	public void setX(int value) {
+		this.x = value;
+	}
+	public void setY(int value) {
+		this.y = value;
+	}
+	
+	public Vector suma(Vector v) {
+		return new Vector(this.x + v.x, this.y + v.y);
+	}
+	
+	public Vector resta(Vector v) {
+		return new Vector(this.x - v.x, this.y - v.y);
+	}
+	
+	public Vector opera(Vector v, char operacion) {
+		Vector result = new Vector (0);
+		if (operacion == '+') {
+			result = this.suma(v);
+			System.out.printf("(%d,%d) + (%d, %d) = (%d,%d)%n", this.x, this.y, v.x, v.y, result.x, result.y);
+			return result;
+		} else if (operacion == '-') {
+			result = this.resta(v);
+			System.out.printf("(%d,%d) - (%d, %d) = (%d,%d)%n", this.x, this.y, v.x, v.y, result.x, result.y);
+			return result;
+		} else {
+			System.out.println("Operación no válida.");
+			return result;
+		}
+	}
+	
+	public Vector prod(int n) {
+		return new Vector(this.x * n, this.y * n);
+	}
+	
+	public Vector opera(int n) {
+		Vector result = this.prod(n);
+		System.out.printf("%d * (%d, %d) = (%d,%d)%n", n, this.x, this.y, result.x, result.y);
+		return result;
+	}
+	
+	public int prodE(Vector v) {
+		return this.x * v.x + this.y * v.y;
+	}
+	
+	public int prodV(Vector v) {
+		return this.x * v.x - this.y * v.y;
+	}
+	
+	public boolean esPerpendicular(Vector v) {
+		int result = this.prodE(v);
+		return result == 0 && (this.x !=0 || this.y != 0) && (v.x != 0 || v.y != 0);
+	}
+	
+	public boolean esParalelo(Vector v) {
+		int result = this.prodV(v);
+		return result == 0 && (this.x !=0 || this.y != 0) && (v.x != 0 || v.y != 0);
+	}
 }
